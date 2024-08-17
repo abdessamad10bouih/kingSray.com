fdd6a0db64857e29aeb4f5740d1d904ebfbadaea
Abdessamad Bouih <151373460+abdessamad10bouih@users.noreply.github.com>
2024-08-17 22:14:16 +0100

    Update login.php

--- backend/login.php
+++ backend/login.php
@@ -1,5 +1,5 @@
 <?php
-header("Access-Control-Allow-Origin: http://localhost:5173/signup");
+header("Access-Control-Allow-Origin: http://kings-ray-com.vercel.app/signup");
 header("Access-Control-Allow-Methods: POST, GET, OPTIONS, DELETE, PUT");
 header("Access-Control-Allow-Headers: Content-Type, Authorization");
 header("Access-Control-Allow-Credentials: true");
