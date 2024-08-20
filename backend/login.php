084abc4556c44deab52b166d7826c22ef86cf790
Abdessamad Bouih <151373460+abdessamad10bouih@users.noreply.github.com>
2024-08-20 23:30:38 +0100

    fix origins

--- backend/login.php
+++ backend/login.php
@@ -1,5 +1,5 @@
 <?php
-header("Access-Control-Allow-Origin: http://localhost:5173/login");
+header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: POST, GET, OPTIONS, DELETE, PUT");
 header("Access-Control-Allow-Headers: Content-Type, Authorization, multipart/form-data");
 header("Access-Control-Allow-Credentials: true");
